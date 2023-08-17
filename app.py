bfef290bc6a77cc88f2b81cdda4a78fe43a5265b
Onda Reki <rekionda0524@ezweb.ne.jp>
2023-08-17 21:57:34 +0900

    htmlのバグ修正

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
 from flask import Flask, render_template
+from flask_sqlalchemy  import SQLAlchemy
+from flaskext.markdown import Markdown
 
+# パイソンで”$ sudo pip3 install Flask-Markdown”を入力してマークダウンを有効にする
 app = Flask(__name__)
+Markdown(app)
 
+# ここでデータベースのURLを指定
+#　db_uri = "mysql+pymysql://root:@charest=utf8"
+#　app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+#　db = SQLAlchemy(app) 
+
+# ここでclassを定義
+# class　POST(db.Model)
+#        __tablename__=""
+#        =db.Column(db.Integer, primary_key=True, autoincrement=True)
+#        =db.Column(db.Integer)
+#        =db.Column(db.Text())
+
+# トップページのルート
 @app.route('/')
 def index():
-    return render_template('index.html')
+    message = "Hello CatScan!!"
+    return render_template('index.html', massage = message )
+
+# 判別結果のルート
+@app.route("/result")
+def result():
+    message = "This Cat!!"
+    return render_template("result.html", massage = message )
+
+#　自分のプロフィールのルート
+@app.route("/profile")
+def profile():
+    message = "Your Profile!!"
+    return render_template("profile.html", message = message )
+
+#　猫図鑑のルート
+@app.route("/book")
+def book():
+    message = "Cats Book!!"
+    return render_template("book.html", message = message )
+
+#　サインインとログイン画面のルートについては後々作成
