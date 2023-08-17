09ad0b878d57ee289b90bdde25455c859dd96d21
yuta-0914 <103375282+yuta-0914@users.noreply.github.com>
2023-08-17 17:02:30 +0900

    Update main.py
    各画面へのルート追加

--- main.py
+++ main.py
@@ -10,7 +10,28 @@ app = Flask(__name__)
 # ここでclassを定義
 # class 
 
+# トップページのルート
 @app.route('/')
 def index():
     message = "Hello CatScan!!"
-    return render_template('index.html', massage = message)
+    return render_template('index.html', massage = message )
+
+# 判別結果のルート
+@app.route("/result")
+def result():
+    message = "This Cat!!"
+    return render_template("result.html", massage = message )
+
+#　自分のプロフィールのルート
+@app.route("/profile")
+def profile():
+    message = "Your Profile!!"
+    return render_template("profile.html", message = message )
+
+#　猫図鑑のルート
+@app.route("/book")
+def book():
+    message = "Cats Book!!"
+    return render_template("book.html", message = message )
+
+#　サインインとログイン画面のルートについては後々作成
