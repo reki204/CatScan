8f0dc8ca022e1b8e5e416d53230c0256f5a4bd7c
yuta-0914 <103375282+yuta-0914@users.noreply.github.com>
2023-08-17 16:27:41 +0900

    Update main.py
    データベースに接続するためのコードを準備

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy  import SQLAlchemy
 app = Flask(__name__)
+
+# ここでデータベースのURLを指定
+#　db_uri = "mysql+pymysql://root:@charest=utf8"
+#　app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+#　db = SQLAlchemy(app) 
+
+# ここでclassを定義
+# class 
 
 @app.route('/')
 def index():
